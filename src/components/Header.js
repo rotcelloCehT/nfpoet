13e072af5be9add45f5972a18ed6b0c4f4653503
Tony <anthonyradin123@gmail.com>
2021-03-24 12:54:55 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+function Header() {
+    return(
+        <div className="header-container">
+            <div className="image-container">
+                <div className="profile-container"></div>
+            </div>
+            <a target="_blank"  rel="noreferrer"href="https://twitter.com/nfpoet?s=20" className="twitter-handle">@nfpoet</a>
+            <div className="delimiter"></div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Intro.js
@@ -0,0 +1,13 @@
+import './Intro.css';
+function Intro () {
+    return(
+        <div className="intro-container">
+            <div className="intro">
+                <p>A collection of poems represented by NFT's<br></br>For each poem there is one artwork<br></br>Posses the art, posses the poem</p>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Intro;
