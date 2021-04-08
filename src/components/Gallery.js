d0e971bbed3cd08630842983d046fc9f04ddcf29
Tony <anthonyradin123@gmail.com>
2021-04-08 13:35:16 +0200

    stupid mistake

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,11 @@
 import './Gallery.css';
 import { Link } from 'react-router-dom';
 
-function Gallery (props) {
+function Gallery ( {poemList} ) {
     return(
         <div className="gallery-container">
             <div className="gallery-list">
-                { props.props.map((poem, index) => (
+                { poemList.map((poem, index) => (
                     <div key={ poem.key } className="poem-container" data-aos="fade-in" data-aos-duration="3000">
                         <Link to={poem.link}><img src={poem.source} alt={poem.title} width="100px" height="100px" /></Link>
                     </div>
